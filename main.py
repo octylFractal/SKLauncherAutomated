ea802731a4810fd2aa41ba723b5d7547b62aaa59
Kenzie Togami <ket1999@gmail.com>
2015-10-20 18:47:51 -0700

    More patches.

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def write_jsons(downloads, package_name, package_title, package_version, launche
     latest = OrderedDict()
     latest["version"] = launcher_version_full
     latest["url"] = \
-        "http://kenzierocks.github.io/Launcher/data/launcher-{}.jar.pack".format(launcher_version_full)
+        "http://kenzierocks.me/Launcher/data/launcher-{}.jar.pack".format(launcher_version_full)
     packages = OrderedDict()
     packages["minimumVersion"] = 1
     pkg = OrderedDict()
